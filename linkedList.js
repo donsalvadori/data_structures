d69b077d856550c0c040913915c87c2568ecae76
Chris Salvi <csalvi42@gmail.com>
2019-02-13 14:18:02 -0800

    added method

--- linkedList.js
+++ linkedList.js
@@ -23,7 +23,32 @@ LinkList.prototype.addToTail = function(value) {
 	this.tail = newNode;
 };
 
-// var ll = new LinkList();
+LinkList.prototype.removeHead = function() {
+	if(!this.head) return null;
+	var val = this.head.value;
+	this.head = this.head.next;
+	if(this.head) this.head.prev = null;
+	else this.tail = null;
+	return val;
+};
+
+LinkList.prototype.removeTail = function() {
+	if(!this.tail) return null;
+	var val = this.tail.value;
+	this.head = this.head.next;
+	if(this.head) this.head.prev = null;
+	else this.tail = null;
+	return val;
+};
+
+
+// var myLL = new LinkList();
+
+// myLL.addToTail(10);
+// myLL.addToTail(20);
+// myLL.addToTail(30);
+
+// console.log(myLL);
 
 // ll.addToHead(100);
 // ll.addToHead(200);
